74b9b3f64734cb0e2003d9d4c2e6515cb1821d87
initdb <benedict.schwind@live.de>
2018-11-22 14:48:46 +0100

    initial commit of ex 7 with working retrofit API.

--- src/test/java/de/thro/inf/prg3/a07/tests/OpenMensaAPITests.java
+++ src/test/java/de/thro/inf/prg3/a07/tests/OpenMensaAPITests.java
@@ -7,12 +7,19 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -22,8 +29,8 @@ class OpenMensaAPITests {
 	private static final Logger logger = LogManager.getLogger(OpenMensaAPITests.class);
 	private OpenMensaAPI openMensaAPI;
 
-	@BeforeAll
-	void setup() {
+	@BeforeEach
+	void SetUp() {
 
 		// use this to intercept all requests and output them to the logging facilities
 		HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
@@ -44,12 +51,15 @@ class OpenMensaAPITests {
 
 	@Test
 	void testGetMeals() throws IOException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+		String today = sdf.format(new Date());
 		// TODO prepare call
-
+		Call<List<Meal>> mealCall = openMensaAPI.getMeals(today);
 		// TODO execute the call synchronously
+		Response<List<Meal>> mealResponse = mealCall.execute();
 
 		// TODO unwrap the body
-		List<Meal> meals = null;
+		List<Meal> meals = mealResponse.body();
 
 		assertNotNull(meals);
 		assertNotEquals(0, meals.size());
